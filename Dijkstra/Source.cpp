8f90472cb408623559c963eb4e575c86eb19438a
IgnatiusPo <ak-1990@mail.ua>
2018-05-13 17:05:24 +0300

    nothing important

--- Dijkstra/Source.cpp
+++ Dijkstra/Source.cpp
@@ -7,5 +7,21 @@
 #include <crtdbg.h>
 int main()
 {
+<<<<<<< HEAD
 
+=======
+    stl::stl_info info;
+    std::string str = "D://GIT//Repos//Dijkstra-algo-for-stl-files//Examples_stl_files//batarang.stl";
+    info.stl_parse(str);
+    std::vector<stl::point*> vc(info.get_points(-3, 0, 0, 10));
+    std::cout << *(*vc.begin());
+
+    if (_CrtDumpMemoryLeaks())
+        std::cout << "Memory Leaks" << std::endl;
+    else
+        std::cout << "Memory OK" << std::endl;
+
+	
+	return 0;
+>>>>>>> Iga-branch
 }
